7065915764521c33dab14420d5a583c1087ad903
Dominic Farquharson <farquharsondominic@gmail.com>
2017-11-30 15:12:20 -0500

    add loading animation.

--- App.js
+++ App.js
@@ -1,11 +1,42 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import loading from './assets/loading.gif';
+
+const Heading = props => {
+  return (
+    <Text style={styles.heading}>
+      {props.content}
+    </Text>
+  )
+}
+
 
 export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      manga: null,
+      loaded: false
+    }
+
+    // binding
+    this.displayMangas = this.displayMangas.bind(this);
+  }
+  componentDidMount() {
+    
+  }
+
+  displayMangas() {
+    return this.state.loaded?
+      <p>Data</p> 
+      : <Image source={loading} style={styles.loading} />
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Hello world!</Text>
+        <Heading content="Manga List" />
+        {this.displayMangas()}
       </View>
     );
   }
@@ -14,8 +45,18 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'rgb(24, 31, 38)',
     alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 40,
   },
+  loading: {
+    // width: 50,
+    flex:.5
+  },
+  heading: {
+    color: 'white',
+    fontSize: 25,
+    marginTop: 20,
+    marginBottom: 20
+  }
 });
